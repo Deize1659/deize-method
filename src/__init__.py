a55434cf4e8c7b11fe85594ae3f52465cb47e73c
Deize <ddeize.git@gmail.com>
2023-07-25 15:11:42 +0900

    [add]ディレクトリ追加メソッド追加

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
+from .directory_create import directory_create
 from .directory_listing import directory_listing
 
-__all__ = ["directory_listing"]
+__all__ = ["directory_listing", "directory_create"]
 
 __version__ = "1.0"
