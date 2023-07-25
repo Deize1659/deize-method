b3a71e006e8c88619f49773a70be96d79d80d79d
Deize <ddeize.git@gmail.com>
2023-07-26 08:52:23 +0900

    [fix]configファイル作成

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-import src
-
-NAME = "my-method"
-AUTHOR = "ymorishima030"
-AUTHOR_EMAIL = "ymorishima@hochiki.co.jp"
-DESCRIPTION = "my-method"
-VERSION = src.__version__
-PACKAGES = find_packages()
-PY_REQUIRES = ">=3.11"
-
-setup(
-    name=NAME,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    maintainer=AUTHOR,
-    maintainer_email=AUTHOR_EMAIL,
-    version=VERSION,
-    packages=PACKAGES,
-    python_requires=PY_REQUIRES,
-)
+setup()
