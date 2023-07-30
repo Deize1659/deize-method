f8c0451f5a6e5490e033501bfc01dfec2bc6ca82
Deize <ddeize.git@gmail.com>
2023-07-30 23:40:20 +0900

    [add]正規表現のファイル名検索の追加

--- my_method/common.py
+++ my_method/common.py
@@ -1,2 +1,3 @@
 import os  # noqa F401
+import re  # noqa F401
 from typing import Union  # noqa F401
--- my_method/directory_listing.py
+++ my_method/directory_listing.py
@@ -1,18 +1,22 @@
-from .common import os
+from .common import os, re
 
 
 def directory_listing(
-    ext: tuple[str] = (""), exc_ext: tuple[str] = ("")
+    name: str = "",
+    ext: str | tuple[str] = (""),
+    exc_ext: str | tuple[str] = (""),
 ) -> list[str]:
     """
     カレントディレクトリのリスト化
 
     Parameters
     ----------
+    name : str, optional
+        正規表現を含めたファイル名, by default ""
     ext : tuple[str], optional
-        拡張子, by default ""
+        拡張子, by default ("")
     exc_ext : tuple[str], optional
-        除外拡張子, by default ""
+        除外拡張子, by default ("")
 
     Returns
     -------
@@ -20,23 +24,36 @@ def directory_listing(
         リスト化したディレクトリ一覧
     """
     path = "."
-    if ext != (""):
-        files = [
-            file
-            for file in os.listdir(path)
-            if os.path.isfile(os.path.join(path, file)) and file.endswith(ext)
-        ]
-    elif exc_ext != (""):
-        files = [
-            file
-            for file in os.listdir(path)
-            if os.path.isfile(os.path.join(path, file))
-            and not file.endswith(exc_ext)
-        ]
+    if name == "":
+        if ext != (""):
+            files = [
+                file
+                for file in os.listdir(path)
+                if os.path.isfile(os.path.join(path, file))
+                and file.endswith(ext)
+            ]
+        elif exc_ext != (""):
+            files = [
+                file
+                for file in os.listdir(path)
+                if os.path.isfile(os.path.join(path, file))
+                and not file.endswith(exc_ext)
+            ]
+        else:
+            files = [
+                file
+                for file in os.listdir(path)
+                if os.path.isfile(os.path.join(path, file))
+            ]
     else:
         files = [
             file
             for file in os.listdir(path)
             if os.path.isfile(os.path.join(path, file))
         ]
+        temp = []
+        for file in files:
+            if re.fullmatch(name, file):
+                temp.append(file)
+        files = temp
     return files
