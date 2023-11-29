7b595ec66d0a0235b66c9311e8e6c9632f027002
Deize <ddeize.git@gmail.com>
2023-11-29 15:32:29 +0900

    [update]directory_listingアップデート

--- my_method/__init__.py
+++ my_method/__init__.py
@@ -1,10 +1,10 @@
-from my_method.directory_create import directory_create
-from my_method.directory_listing import directory_listing
-from my_method.my_input import input_float, input_int, input_str
+from .directory_create import directory_create
+from .directory_listing import DirectoryListing
+from .my_input import input_float, input_int, input_str
 
 __all__ = [
-    "directory_listing",
     "directory_create",
+    "DirectoryListing",
     "__version__",
     "input_float",
     "input_int",
